7ca1ad92c6ad1958d43f96f609a328dea08614cf
Ishan Sharma <ishandeveloper@outlook.com>
2020-05-02 00:46:06 +0530

    Added Credits

---
+++ src/components/footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div class="footer">
+        Made with <font color="red">♥</font> by{" "}
+        <a target="_blank" href="https://github.com/ishandeveloper">
+          ishandeveloper{" "}
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
