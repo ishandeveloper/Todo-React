5152dc7d6938287037815f32eea91e8ff1d29644
Ishan Sharma <ishandeveloper@outlook.com>
2020-05-02 00:30:10 +0530

    Main App

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "./components/form";
+// import List from './components/list';
+function App() {
+  return (
+    <div className="todo-table">
+      <Form />
+      {/* <List/> */}
+    </div>
+  );
+}
+
+export default App;
